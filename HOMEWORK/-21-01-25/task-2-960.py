26d9257a799986f8cdf638df8d95b48b27bd512c
PetrickBateman <dhudonogov2001@gmail.com>
2025-01-25 23:29:26 +0200

    task-2-960

---
+++ HOMEWORK/-21-01-25/task-2-960.py
@@ -0,0 +1,19 @@
+from itertools import product, permutations
+
+def f(a, b, c):
+    return a and (not b) or c
+
+table = [
+    (0, 0, 0),
+    (0, 0, 1),
+    (0, 1, 0),
+    (0, 1, 1),
+    (1, 0, 0),
+    (1, 0, 1),
+    (1, 1, 0),
+    (1, 1, 1)
+]
+for p in permutations('abc'):
+    u = [f(**dict(zip(p, t))) for t in table] == [0, 1, 1, 1, 0, 0, 1, 1]
+    if u:
+        print(*p)
