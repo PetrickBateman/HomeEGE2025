80ee696dd14d33c97130d67b364a9a31d667ef46
PetrickBateman <dhudonogov2001@gmail.com>
2025-01-31 17:00:42 +0200

    task-15-homew

---
+++ HOMEWORK/-31-01-2025/task-15-19247.py
@@ -0,0 +1,11 @@
+def f(A):
+    for x in range(1, 1000):
+        for y in range(1, 1000):
+            f = (x - 3 * y < A) or (y > 400) or (x > 56)
+            if not f:
+                return False
+    return True
+for A in range(1, 10000):
+    if f(A):
+        print(A)
+        break
