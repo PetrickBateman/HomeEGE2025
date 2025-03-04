52fb822cbdf4bb6f04debd20139212d6c2c516b2
PetrickBateman <dhudonogov2001@gmail.com>
2025-03-04 18:32:34 +0200

    04-04-domashka

---
+++ 04-03-25/task-5-19872.py
@@ -0,0 +1,20 @@
+def convert(num, sys):
+    res = ''
+    while num:
+        res += str(num % sys)
+        num //= sys
+    return res[::-1]
+
+ans = []
+for n in range(1, 1001):
+    r = convert(n, 7)
+    if n % 2 == 0:
+        r = '1' + r + '52'
+    else:
+        r = r[-1] + r[1:-1] + r[0] + '15'
+    for i in range(len(r)):
+        if r[0] == '0':
+            r = r[1:]
+        if len(r) == 4:
+            ans.append(n)
+print(max(ans))
